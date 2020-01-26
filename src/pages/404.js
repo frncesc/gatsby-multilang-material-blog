27a584021e20e6ddcfb6a374795758ff257b818d
Francesc Busquets <francesc@gmail.com>
2020-01-26 17:56:34 +0100

    :bug: Avoid 'window' in SSR

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { useIntl } from 'gatsby-plugin-intl';
 import { makeStyles } from '@material-ui/core/styles';
 import Layout from '../components/Layout';
@@ -15,13 +15,19 @@ export default function NotFoundPage() {
   const intl = useIntl();
   const { locale: lang, formatMessage } = intl;
   const classes = useStyles();
+  const [path, setPath] = useState('');
+
+  useEffect(()=>{
+    if(window)
+      setPath(window.location.pathname);
+  });
 
   return (
     <Layout {...{ intl }}>
       <SEO title="404: Not Found" {...{ lang }} />
       <article className={classes.root}>
         <h1>{intl.messages['404-not-found']}</h1>
-        <p>{formatMessage({ id: 'not-found' }, { path: window.location.pathname })}</p>
+        <p>{formatMessage({ id: 'not-found' }, { path })}</p>
       </article>
     </Layout>
   );
