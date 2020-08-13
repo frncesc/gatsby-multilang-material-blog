499a3927b51d88e5e40dd579f0fb6c969a604a90
Francesc Busquets <francesc@gmail.com>
2020-08-13 11:40:26 +0200

    :arrow_up: Update components

--- src/utils/misc.js
+++ src/utils/misc.js
@@ -18,9 +18,17 @@ export const mergeClasses = (props, classes, root = 'root') => {
  */
 export const isAbsoluteUrl = (text) => text && /^https?:\/\//.test(text);
 
+/**
+ * Checks if the given expression is an absolute path
+ * (paths starting with '/')
+ * @param {string} text - The expression to check
+ */
+export const isAbsolutePath = (text) => text && /^\//.test(text);
+
 export const getImgUrl = ({ siteMetadata: { baseUrl, pathPrefix, cardFileName }, slug, lang, thumbnail = null }) => {
   return isAbsoluteUrl(thumbnail) ? thumbnail
-    : thumbnail?.childImageSharp?.fluid?.src ? `${baseUrl}${thumbnail.childImageSharp.fluid.src}`
-      : slug ? `${baseUrl}${pathPrefix}/${lang}${slug}${cardFileName}`
-        : null;
+    : isAbsolutePath(thumbnail) ? `${baseUrl}${pathPrefix}${thumbnail}`
+      : thumbnail?.childImageSharp?.fluid?.src ? `${baseUrl}${thumbnail.childImageSharp.fluid.src}`
+        : slug ? `${baseUrl}${pathPrefix}/${lang}${slug}${cardFileName}`
+          : null;
 }
